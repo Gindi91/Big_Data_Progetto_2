e578f4df0cb3bcbcfccd5597f194ee0730042f8f
Gindi91 <gdini91@gmail.com>
2018-07-17 12:29:34 +0200

    Update vuln.py

--- vuln.py
+++ vuln.py
@@ -1,11 +1,10 @@
 import sys
 from pyspark import SparkContext, SparkConf
 
-conf=SparkConf().setAppName("Controllo delle vulnerabilità")
+conf=SparkConf().setAppName("Controllo delle vulnerabilita")
 sc=SparkContext(conf=conf)
-text_file=sc.textFile("hdfs://localhost:9000/user/gindi/input/file_rou_20180311_mini.txt")
 
-result=text_file.map(lambda line: line.split(";")).filter(lambda line: len(line[6])==1).map(lambda line: (line[6],1)).reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[0], False).collect()
+result=sc.textFile("hdfs://localhost:9000/user/gindi/input/file_rou_20180311_mini.txt").map(lambda line: line.split(";")).filter(lambda line: len(line[6])==1).map(lambda line: (line[6],1)).reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[0], False).collect()
 
 print "=========================="
 print result
