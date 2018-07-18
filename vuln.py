857772c056d8b27bdb6d75ec2b5bea6d0c54883e
Fabio Marchionni <marchionni.fabio@gmail.com>
2018-07-18 10:31:42 +0200

    Update vuln.py

--- vuln.py
+++ vuln.py
@@ -10,13 +10,13 @@ pathFile = "hdfs://localhost:9000/hduser/input/" + FileInput
 #Funzione per ricavare il numero di ripetitori per ciascun contatore
 def count_reps(line):
 	if line[3] is None or line[3]=="               ":
-		return 1
-	elif line[4] is None  or line[4]=="               ":
 		return 2
-	elif line[5] is None  or line[5]=="               ":
+	elif line[4] is None  or line[4]=="               ":
 		return 3
-	else:
+	elif line[5] is None  or line[5]=="               ":
 		return 4
+	else:
+		return 5
 
 #Controllo di validita del record rappresentato da line
 def is_valid(line):
@@ -35,17 +35,11 @@ sc=SparkContext(conf=conf)
 text_file=sc.textFile(pathFile).map(lambda line: line.split(";")).filter(lambda line: is_valid(line)==1)
 
 #Calcolo del numero di contatori per livello di vulnerabilita
-vuln=text_file.map(lambda line: (count_reps(line),1)).reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[0], False).saveAsTextFile(fileRisultato)
+vuln=text_file.map(lambda line: (count_reps(line),1)).reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[0], False)
 
 #Calcolo della vulnerabilita totale media del sistema
 vuln_map=text_file.map(lambda line: (1, count_reps(line))).values()
-mean_vuln=float(vuln_map.sum())/float(vuln_map.count())
+mean_vuln=sc.parallelize(["Vulnerabilita totale media del sistema:",float(vuln_map.sum())/float(vuln_map.count())])
 
-#Rappresentazione dei risultati
-print "=========================="
-print "Vulnerabilita totale media del sistema"
-print mean_vuln
-print "=========================="
-for record in vuln:
-	print record
-print "=========================="
+#Unione dei due risultati parziali e scrittura dei file su HDFS
+output = sc.union([mean_vuln,vuln]).saveAsTextFile(fileRisultato)
