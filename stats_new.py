575a04e88599f688b9ba4f7b7e0196740915b7e7
Gindi91 <gdini91@gmail.com>
2018-07-18 16:18:35 +0200

    Add files via upload

---
+++ stats_new.py
@@ -0,0 +1,37 @@
+import sys
+import time
+from pyspark import SparkContext, SparkConf
+
+#Controllo di validita del record rappresentato da line
+def is_valid(line):
+	if len(line[0])==13:
+		return 1
+	else:
+		return -1
+
+#Funzione che associa una fascia per ogni valore
+def statistics(num):
+	if int(num)<86:
+		return 85
+	else:
+		return int(num)
+
+#Nome del file di output
+today = time.strftime("%Y%m%d-%H%M%S")
+#fileRisultato = "hdfs://localhost:9000/hduser/output/stats_new_" + today
+fileRisultato = "hdfs://localhost:9000/user/gindi/output/stats_new_" + today
+
+#Configurazione iniziale spark
+conf=SparkConf().setAppName("Analisi statistiche")
+sc=SparkContext(conf=conf)
+
+#Unione dei files in input
+rdd=sc.textFile("file:///home/gindi/spark-2.3.0-bin-hadoop2.7/bin/jars/Input/COM/*.NEW")
+#rdd = sc.textFile("file:///usr/local/spark/input/COM/*.NEW")
+text_file=rdd.coalesce(1).map(lambda line: line.split(";")).filter(lambda line: is_valid(line)==1)
+
+#Creazione dell'output in rdd
+stats_map=text_file.map(lambda line: (statistics(line[3]),1)).reduceByKey(lambda x,y:x+y).sortBy(lambda x: x[0], False)
+
+#Creazione del file di output
+output=stats_map.saveAsTextFile(fileRisultato)
