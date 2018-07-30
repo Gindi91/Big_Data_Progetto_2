a7ce8d548ef99e843c935918a45a2c66617424d9
Gindi91 <gdini91@gmail.com>
2018-07-30 11:03:20 +0200

    Update stats.py

--- stats.py
+++ stats.py
@@ -37,5 +37,8 @@ text_file=rdd.coalesce(1).map(lambda line: line.split("(")).filter(lambda line: 
 #Creazione dell'output in rdd
 stats_map=text_file.map(lambda line: (statistics(ex_num(line)),1)).reduceByKey(lambda x,y:x+y).sortBy(lambda x: x[0], False)
 
+#Creazione del log dei record non validi
+not_valid=rdd.coalesce(1).map(lambda line: line.split("(")).filter(lambda line: is_valid(line)==-1)
+
 #Creazione del file di output
-output=stats_map.saveAsTextFile(fileRisultato)
+output = sc.union([stats_map,not_valid]).saveAsTextFile(fileRisultato)
